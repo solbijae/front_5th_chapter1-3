95bdf17cea1430de9099ec2b275f8104213e39ae
Jae <jaeyeonjung3@gmail.com>
2025-04-09 22:12:01 +0900

    [Refactor] useMemo에 주석 추가

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -3,19 +3,19 @@ import { shallowEquals } from "../equalities";
 import { useRef } from "./useRef";
 
 export function useMemo<T>(
-  factory: () => T,
-  _deps: DependencyList,
-  _equals = shallowEquals,
+  factory: () => T, // 재사용하고 싶은 계산 함수
+  _deps: DependencyList, // 의존성 배열
+  _equals = shallowEquals, // 값을 비교하는 함수
 ): T {
-  const depsRef = useRef<DependencyList | undefined>(undefined);
-  const valueRef = useRef<T | undefined>(undefined);
+  const depsRef = useRef<DependencyList | undefined>(undefined); // 의존성 배열을 저장할 ref
+  const valueRef = useRef<T | undefined>(undefined); // 계산 결과를 저장할 ref
 
-  const hasNoDeps = !depsRef.current;
-  const depsChanged = !_equals(_deps, depsRef.current || []);
+  const hasNoDeps = !depsRef.current; // 처음 실행하는 경우
+  const depsChanged = !_equals(_deps, depsRef.current || []); // 의존성 배열이 변경된 경우
 
   if (hasNoDeps || depsChanged) {
-    valueRef.current = factory();
-    depsRef.current = _deps;
+    valueRef.current = factory(); // 새로운 값을 계산하고 저장
+    depsRef.current = _deps; // depsRef를 업데이트해서 다음 비교 준비
   }
 
   return valueRef.current as T;
